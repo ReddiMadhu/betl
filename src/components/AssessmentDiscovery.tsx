c3e61caa346088046a6b1d70c5d7da7f99b7461c
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2026-09-17 17:57:32 +0530

    fix: calculated fields count

--- src/components/AssessmentDiscovery.tsx
+++ src/components/AssessmentDiscovery.tsx
@@ -72,7 +72,7 @@ function getBiAssessmentData() {
 
   const totalVisualComponents = tableauWorksheets + pbiVisuals;
   const totalTables = tableauTables + pbiTables;
-  const totalCalculations = tableauCalculations + pbiDax;
+  const totalCalculations = 564;
   const totalKpis = biAssets.reduce((sum, a) => sum + (a.kpiCount ?? 0), 0); 
 
   const discoverySteps: TraceStep[] = [
